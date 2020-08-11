7793928fc8db3614849e5cd97c61e0203dc0dab9
Suresh Ramaswamy <suresh.ramaswamy@gmail.com>
2020-08-11 16:15:23 -0700

    added ditto for clipboard history

--- win_choc_install.bat
+++ win_choc_install.bat
@@ -6,11 +6,12 @@ REM
 
 REM ### backup/sync
 choco install dropbox evernote -y
+
 REM # checksum error
 choco install google-backup-and-sync -y --ignore-checksums
 
 REM #### utils
-choco install sysinternals autohotkey.portable autoruns sharpkeys launchy windirstat winmerge pdftk 7zip -y
+choco install sysinternals autohotkey.portable autoruns sharpkeys launchy windirstat winmerge pdftk 7zip ditto -y
 
 REM #### programming
 choco install -y python3 curl git putty
