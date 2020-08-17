95462f93d534ab8bb92133d8f331deec88a8d1b8
suresh-ramaswamy <suresh-ramaswamy@users.noreply.github.com>
2020-08-17 16:04:46 -0700

    Comment out zsh related install steps

--- install.sh
+++ install.sh
@@ -14,19 +14,19 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
+# Symlink the Mackup config file to the home directory
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Make ZSH the default shell environment
 # TODO: do this only if zsh is not the current shell!
-chsh -s $(which zsh)
+#chsh -s $(which zsh)
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+#rm -rf $HOME/.zshrc
+#ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
